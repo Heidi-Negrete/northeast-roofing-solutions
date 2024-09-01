8f0fdb6d6093ac11c09cd6045a7c367039f124ef
Heidi Negrete <heidi@negrete.dev>
2024-09-01 10:51:58 -0400

    update config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-// https://astro.build/config
+// https://astro.build/config TO DO SETUP W DEPLOYMENT
 export default defineConfig({
-  site: "https://northeastroofingsolutions.com",
+  site: "https://heidi-negrete.github.io",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
